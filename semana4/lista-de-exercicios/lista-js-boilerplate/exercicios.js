a5dc6c88ccf67caedd93060bf5bcb62bd27732e5
Janaylla Firmino <janayllaf@gmail.com>
2021-03-05 11:37:37 -0300

    Exercicio 5

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -33,8 +33,8 @@ function retornaMaiorNumero(array) {
    // implemente sua lógica aqui
    let maior = array[0];
    for(let i=1; i<array.length; i++ ){
-      if(array[i]>maior )
-      maior = array[i]
+      if(array[i]>maior)
+      maior = array[i];
    }
    return maior;
 }
@@ -43,6 +43,7 @@ function retornaMaiorNumero(array) {
 
 function retornaQuantidadeElementos (array) {
    // implemente sua lógica aqui
+   return array.length;
 }
 
 //Exercício 6

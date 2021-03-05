576285f0986575092bb3483839ecfc06f1ab2665
Janaylla Firmino <janayllaf@gmail.com>
2021-03-05 12:02:03 -0300

    Exercicio 6

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -50,6 +50,20 @@ function retornaQuantidadeElementos (array) {
 
 function retornaExpressoesBooleanas() {
    // implemente sua lógica aqui
+   const booleano1 = true;
+   const booleano2 = false;
+   const booleano3 = !booleano2; 
+   const booleano4 = !booleano3; 
+   const respostas = [];
+
+   respostas.push(booleano1 && booleano2 && !booleano4);
+   respostas.push((booleano1 && booleano2) || !booleano3);
+   respostas.push((booleano2 || booleano3) && (booleano4 || booleano1));
+   respostas.push(!(booleano2 && booleano3) || !(booleano1 && booleano3));
+   respostas.push(!(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3));
+   
+   return respostas;
+
 }
 
 //Exercício 7

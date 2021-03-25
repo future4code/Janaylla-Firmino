64ba94e14223dd44ca568873736a728c7a94ff3c
Janaylla Firmino <janayllaf@gmail.com>
2021-03-25 13:37:37 -0300

    Atv do dia - comentario inicial

---
+++ semana7/APIs-na-Natureza/poke/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+export default class App extends React.Component{
+  render(){
+  return (
+    <div>
+     
+    </div>
+  );
+}
+}
+

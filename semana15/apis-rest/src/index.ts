6736b01ba3692b80cfdd1a709aaf85170d52f9d0
Janaylla Firmino <janayllaf@gmail.com>
2021-05-20 23:58:16 -0300

    Finalizando a atc

--- semana15/apis-rest/src/index.ts
+++ semana15/apis-rest/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { send } from 'process'
 
 type User = {
   id: number,
@@ -59,6 +60,122 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+app.get("/users/all", (req: Request, res: Response) => {
+  res.status(200).send(users)
+  // *a. Qual método HTTP você deve utilizar para isso?*
+// GET
+// *b. Como você indicaria a **entidade** que está sendo manipulada?*  
+// Pele url
+})
+
+app.get("/users/type/:type", (req: Request, res: Response) => {
+//   *a.* Como você passou os parâmetros de type para a requisição? Por quê?
+// pela url porque n é uma infomação confidencial e unica
+// b. Você consegue pensar em um jeito de garantir que apenas `types` válidos sejam utilizados?
+// com o if e n fiz distinção entre letra maiusculas e minussculas
+  try{
+    const {type} = req.params;
+    if(type.toUpperCase() != "NORMAL" && type.toUpperCase() != "ADMIN"){
+      throw new Error("Type something");
+    }else{
+      res.status(200).send(
+        users.filter(user => user.type = type.toUpperCase())
+      );
+    }
+  }
+  catch(error){
+    res.status(400).send({ message: error.message });
+  }
+
+})
+
+app.get("/users/name/:name", (req: Request, res: Response) => {
+  
+  // *a. Qual é o tipo de envio de parâmetro que costuma ser utilizado por aqui?*
+  // query parametros
+
+    try{
+      const {name} = req.params;
+
+      if(name && typeof(name) === "string"){
+       let result = users.filter(user =>
+         user.name.toLowerCase().includes(name.toLowerCase())
+         )
+         if(result.length > 0){
+          res.status(400).send(users);
+         }
+         else{
+          throw new Error("No users were found");
+         }
+        
+      }else{
+        throw new Error("Type something");
+      }
+    }
+    catch(error){
+      res.status(400).send({ message: error.message });
+    }
+  
+  })
+  
+app.post("/user", (req: Request, res: Response) => {
+    try{
+      const {
+           age,
+           email,
+           id,
+           name,
+           type 
+      }:User = req.body;
+      const newUser:User = { 
+        id,
+        name,
+        age,
+        type,
+        email
+      }
+      if(users.push(newUser)){
+        res.status(200).send({ message: "new user added" });
+      }
+    }
+    catch(error){
+      res.status(400).send({ message: error.message });
+    }
+  
+  })
+    
+app.put("/user", (req: Request, res: Response) => {
+  // a. Mude o método do endpoint para `PUT`. O que mudou?
+  // Na hora de escrever o codigo apenas a palavra. Porem as requisições tem comportamentos diferente
+  // b. Você considera o método `PUT` apropriado para esta transação? Por quê?
+  // Sim, porque essa requisição altera uma lista, e se n quisermos varias ocorrendo ao mesmo tempo o put é uma boa opção
+
+  try{
+    const {
+         age,
+         email,
+         id,
+         name,
+         type 
+    }:User = req.body;
+    
+    const newUser:User = { 
+      id,
+      name,
+      age,
+      type,
+      email
+    }
+    if(users.push(newUser)){
+      res.status(200).send({ message: "new user added" });
+    }
+  }
+  catch(error){
+    res.status(400).send({ message: error.message });
+  }
+
+})
 // Para testar se o servidor está tratando os endpoints corretamente
 app.get("/ping", (req: Request, res: Response) => {
   res.status(200).send("pong!")

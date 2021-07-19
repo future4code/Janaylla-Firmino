1d2e3907fc5128143bfc038fe41cac1f7b7dde97
Janaylla Firmino <janayllaf@gmail.com>
2021-07-19 18:32:26 -0300

    Finalizando a atv

--- semana24/resolvendo-problemas/src/exercise1/index.ts
+++ semana24/resolvendo-problemas/src/exercise1/index.ts
@@ -1,32 +1,21 @@
-/*
+function isOneEdit(string1: string, string2: string): boolean {
+  if (Math.abs(string2.length - string1.length) > 1) {
+    return false
+  }
 
-Função que receba uma string e checa se ela
-é um palíndromo
+  if (string1.length > string2.length) {
+    return string1.includes(string2)
+  }
+  if (string2.length > string1.length) {
+    return string2.includes(string1)
+  }
 
-1. Receber a string
-2. Limpar a string // regex
-3. Deixar tudo minúsculo // toLowerCase
-4. Inverter // split("") reverse() join("")
-5. Comparar 
-
-Bob.
-A dama admirou o rim da amada.
-Aí, Lima falou: "Olá, família".
-
-*/
-
-import { normalizer } from "../utils/normalizer"
-
-const isPalindrome = (text: string): boolean => {
-  const cleanText = normalizer(text)
-  const invertedText = cleanText.split("").reverse().join("")
-  return cleanText === invertedText
+  let charsDiffCount = 0
+  for (let i = 0; i < string1.length; i++) {
+    if (string1[i] !== string2[i]) {
+      charsDiffCount++
+    }
+  }
+  return charsDiffCount === 1
 }
-
-console.log(isPalindrome(`Bob.`)) 
-console.log(isPalindrome(`A dama admirou o rim da amada.`))
-console.log(isPalindrome(`Aí, Lima falou: "Olá, família"!`))
-
-console.log(isPalindrome(`Bobz!`))
-
-
+console.log(isOneEdit("banan", "banana"))
--- semana24/resolvendo-problemas/src/exercise2/index.ts
+++ semana24/resolvendo-problemas/src/exercise2/index.ts
@@ -1,90 +1,17 @@
-/*
-
-Dadas duas strings, checar se são anagramas
-
-1. Receber duas strings
-2. Limpar as strings // reutilizar a função normalizer
-3. Comparar quantidade de letras // length
-4. Ordenar alfabeticamente // split sort join
-5. Comparar
-
-Alegria: galeria
-Roma: amor
-Céu: uec
-
-bob: bobb
-
-*/
-
-import { normalizer } from "../utils/normalizer"
-
-interface HashTable {
-  [index: string]: number
-}
-
-const isAnagram = (
-  firstText: string,
-  secondText: string
-): boolean => {
-  const cleanFirstText = normalizer(firstText)
-  const cleanSecondText = normalizer(secondText)
-
-  if (cleanFirstText.length !== cleanSecondText.length) {
-    return false
-  }
-
-  const sortedFirst = cleanFirstText.split("").sort().join("")
-  const sortedSecond = cleanSecondText.split("").sort().join("")
-
-  return sortedFirst === sortedSecond
-}
-
-const isAnagramHashTable = (
-  firstText: string,
-  secondText: string
-): boolean => {
-  const cleanFirstText = normalizer(firstText)
-  const cleanSecondText = normalizer(secondText)
-
-  if (cleanFirstText.length !== cleanSecondText.length) {
-    return false
-  }
-
-  const getHashTable = (str: string): HashTable => {
-    const result: HashTable = {}
-
-    for (let char of str) {
-      if (result[char]) {
-        result[char]++
-      } else {
-        result[char] = 1
+function stringCompression(string: string): string {
+  let stringCompression: string = "";
+    for (let i = 0; i < string.length; i++) {
+      let count: number = 0;
+      const letter: string = string.substr(i, 1)
+      const index = i;
+      for (let j = 0; letter == string.substr(index + j, 1); i++, j++) {
+        count++;
+        console.log(letter)
       }
+      stringCompression += letter + count;
     }
 
-    return result
-  }
 
-  const firstHashTable = getHashTable(cleanFirstText)
-  const secondHashTable = getHashTable(cleanSecondText)
-
-  for (let char in firstHashTable) {
-    if (firstHashTable[char] !== secondHashTable[char]) {
-      return false
-    }
-  }
-
-  return true  
+  return stringCompression
 }
-
-// console.log(isAnagram("Alegria!", "galeria?"))
-// console.log(isAnagram("Roma", "amor."))
-// console.log(isAnagram("Céu!", "uec"))
-
-// console.log(isAnagram("bob!", "bobb?"))
-
-console.log(isAnagramHashTable("Alegria!", "galeria?"))
-console.log(isAnagramHashTable("Roma", "amor."))
-console.log(isAnagramHashTable("Céu!", "uec"))
-
-console.log(isAnagramHashTable("bob!", "bobb?"))
-console.log(isAnagramHashTable("pato!", "bobb?"))
+console.log(stringCompression("aaabbbccaa"))
